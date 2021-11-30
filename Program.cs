7cd1909c9e8c76574e4a524cbad081c537c6e60e
Fynn7w <92077438+Fynn7w@users.noreply.github.com>
2021-11-30 09:56:21 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -18,53 +18,57 @@ namespace PasswordManager
 
         public static void encrypt(string data, string pwd)
         {
-            
+
         }
 
         public static void decrypt(string a)
         {
-            
+
         }
 
-        public static void check_pwd(string pwd)
+        public static bool check_pwd()
         {
 
+
+        return true;
         }
 
         public static void Main(string[] args)
         {
-            Console.WriteLine("****************")
+            Console.WriteLine("****************");
             Console.WriteLine("Password Manager");
-            Console.WriteLine("****************")
-            
+            Console.WriteLine("****************");
+
+
             Console.WriteLine("Enter the Password to Decrypt the Database :");
-            if (check_psswd == true)//Checkt ob passwort/key richtig eingegebn wurde
+            if ( check_pwd() == true)//Checkt ob passwort/key richtig eingegebn wurde
             {
                 //alle optionen die der Nutzer hat
-                string user_input;
-                Console.WriteLine("----------------------------------")
-                Console.WriteLine("[1] Show all passwords")
-                Console.WriteLine("[2] Add a Password to the Database")
-                Console.WriteLine("[3] Exit")
-                Console.WriteLine("----------------------------------")
+                int user_input;
+                user_input = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("----------------------------------");
+                Console.WriteLine("[1] Show all passwords");
+                Console.WriteLine("[2] Add a Password to the Database");
+                Console.WriteLine("[3] Exit");
+                Console.WriteLine("----------------------------------");
                 //1
                 if (user_input == 1)
                 {
-                    open(string_path);
+                    open(path);
                 }
                 //2
                 if (user_input == 2)
                 {
-                add_pwd(string path, string pwd);
+                    add_pwd(string path, string pwd);
                 }
                 //3
                 if (user_input == 3)
                 {
-                    Console.WriteLine("Quitting...")
-                    Environment.Exit()//Methode um programm zu beenden
+                    Console.WriteLine("Quitting...");
+                    Environment.Exit(0);//Methode um programm zu beenden
                 }
-            
-            
+
+
             }
         }
     }
