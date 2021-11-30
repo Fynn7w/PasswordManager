195bc1e57c13fe8f160fa85826d642323ab4097a
Fynn7w <92077438+Fynn7w@users.noreply.github.com>
2021-11-30 09:35:59 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -33,7 +33,39 @@ namespace PasswordManager
 
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("****************")
+            Console.WriteLine("Password Manager");
+            Console.WriteLine("****************")
+            
+            Console.WriteLine("Enter the Password to Decrypt the Database :");
+            if (check_psswd == true)//Checkt ob passwort/key richtig eingegebn wurde
+            {
+                //alle optionen die der Nutzer hat
+                string user_input;
+                Console.WriteLine("----------------------------------")
+                Console.WriteLine("[1] Show all passwords")
+                Console.WriteLine("[2] Add a Password to the Database")
+                Console.WriteLine("[3] Exit")
+                Console.WriteLine("----------------------------------")
+                //1
+                if (user_input == 1)
+                {
+                    open(string_path);
+                }
+                //2
+                if (user_input == 2)
+                {
+                add_pwd(string path, string pwd);
+                }
+                //3
+                if (user_input == 3)
+                {
+                    Console.WriteLine("Quitting...")
+                    Environment.Exit()//Methode um programm zu beenden
+                }
+            
+            
+            }
         }
     }
 }
