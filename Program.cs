34ee2de3f4d7c729e04d9787adc1e3d48239183d
Marek <marek.mueller04@gmail.com>
2021-11-30 08:49:10 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PasswordManager
+{
+    public class Program
+    {
+        public static void open(string path)
+        {
+
+        }
+
+        public static void add_pwd(string path, string pwd)
+        {
+
+        }
+
+        public static void encrypt(string data, string pwd)
+        {
+            
+        }
+
+        public static void decrypt(string a)
+        {
+            
+        }
+
+        public static void check_pwd(string pwd)
+        {
+
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
